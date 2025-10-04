e5bb7dfe3a2ae0cf83229274ddcb1d564d93fd1c
dimastatz <dima.statz@gmail.com>
2025-10-04 08:16:35 +0300

    download name

--- flatr/flatr.py
+++ flatr/flatr.py
@@ -1,21 +1,24 @@
 """ Flattens github repo """
 import os
 import tempfile
-
-import urllib.parse as p
 import requests as r
 
 
 def download(url: str) -> str:
-    """Downloads GitHub repo as zip and extracts it"""
+    """Downloads GitHub repo as zip and extracts it
+    example url: https://github.com/dimastatz/whisper-flow.git
+    downloaded artifact is: https://github.com/dimastatz/whisper-flow/archive/refs/heads/main.zip
+    """
     temp_dir = tempfile.mkdtemp()
-    zip_path = os.path.join(temp_dir, f"{p.urlparse(url).path.split('/')[-1]}.zip")
+    repo_name = url.split("/")[-1].removesuffix(".git")
+    url = url.removesuffix(".git") + "/archive/refs/heads/main.zip"
+    zip_path = os.path.join(temp_dir, f"{repo_name}.zip")
 
-    with r.get(f"{url}/archive/refs/heads/main.zip", timeout=120) as req:
+    with r.get(url, timeout=120) as req:
         with open(zip_path, "wb") as file:
             file.write(req.raise_for_status() or req.content)
 
-    return temp_dir
+    return zip_path
 
 
 def execute(url: str) -> str:
