e16da626e4ad1c4a247f545b383927137dadc3a0
dimastatz <dima.statz@gmail.com>
2025-10-04 14:21:31 +0300

    refactor

--- flatr/flatr.py
+++ flatr/flatr.py
@@ -10,10 +10,9 @@ def download(url: str) -> str:
     example url: https://github.com/dimastatz/whisper-flow.git
     downloaded artifact is: https://github.com/dimastatz/whisper-flow/archive/refs/heads/main.zip
     """
-    temp_dir = tempfile.mkdtemp()
     repo_name = url.split("/")[-1].removesuffix(".git")
     url = url.removesuffix(".git") + "/archive/refs/heads/main.zip"
-    zip_path = os.path.join(temp_dir, f"{repo_name}.zip")
+    zip_path = os.path.join(tempfile.mkdtemp(), f"{repo_name}.zip")
 
     with r.get(url, timeout=120) as req:
         with open(zip_path, "wb") as file:
