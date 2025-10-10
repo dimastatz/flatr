07322c7e7352129634960a9401eee9baad95a741
dimastatz <dima.statz@gmail.com>
2025-10-10 06:44:42 +0300

    add explain

--- flatr/explain.py
+++ flatr/explain.py
@@ -1,11 +1,10 @@
 """Explains Codebase from Flattened Markdown"""
 import os
 import sys
+from ctransformers import AutoModelForCausalLM
 
 
 if __name__ == "__main__":  # pragma: no cover
-    print("flatr Explain \n")
-
     if len(sys.argv) != 2:
         print("Usage: python -m flatr.explain <github_repo_url>")
         sys.exit(1)
@@ -17,3 +16,13 @@ if __name__ == "__main__":  # pragma: no cover
         sys.exit(1)
 
     print(f"model={model_path}")
+
+    llm = AutoModelForCausalLM.from_pretrained(
+        model_path,
+        model_type="llama",
+        context_length=2048,
+        max_new_tokens=512,
+        gpu_layers=0 
+    )
+
+    print(llm("What is your model name", temperature=0.7))
